15228b21ff8584c1989d65b5ff5f09f95c74427f
tofetpuzo <tofet.femi@gmail.com>
2026-06-21 09:05:05 +0100

    updated the eventstore file

--- MoniePay/src/models/EventStore.cs
+++ MoniePay/src/models/EventStore.cs
@@ -9,7 +9,6 @@ namespace MoniePay.src.models
         public string EventType { get; set; } = string.Empty;
         public required JsonDocument Payload { get; set; }
         public DateTime CreateAt { get; set; }
-
         public EventStore() { }
 
         public EventStore(Guid id, Guid aggregateId, string eventType, JsonDocument payload, DateTime createdAt)
