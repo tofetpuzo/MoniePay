001e63a126a1cc3e584dc9985101b6838297d8c6
tofetpuzo <tofet.femi@gmail.com>
2026-06-21 21:44:53 +0100

    modifiied registered file

--- MoniePay/src/services/RegisterUser.cs
+++ MoniePay/src/services/RegisterUser.cs
@@ -1,21 +1,21 @@
-﻿namespace MoniePay.src.services
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace MoniePay.src.services
 {
-    public class RegisterUser
+    public sealed class RegisterUser
     {
-        public required string Username;
-        public required string Password;
-        public required string Email;
-        public RegisterUser(UserDetails userDetails)
-        {
-            Username = userDetails.username;
-            Password = userDetails.password;
-            Email = userDetails.email;
-        }
+        [JsonPropertyName("username")]
+        [Required(ErrorMessage = "username is required")]
+        public required string Username { get; set; }
+
+        [JsonPropertyName("password")]
+        [Required(ErrorMessage = "password is required")]
+        public required string Password { get; set; }
+
+        [JsonPropertyName("email")]
+        [Required(ErrorMessage = "email is required")]
+        [EmailAddress(ErrorMessage = "email must be a valid email address")]
+        public required string Email { get; set; }
     }
-    public sealed record UserDetails
-    (
-        string username,
-        string password,
-        string email
-    );
 }
