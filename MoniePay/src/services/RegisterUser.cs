28141159b891d9455000846b5b09c32c12b37924
tofetpuzo <tofet.femi@gmail.com>
2026-06-21 09:13:50 +0100

    update the register user

---
+++ MoniePay/src/services/RegisterUser.cs
@@ -0,0 +1,21 @@
+﻿namespace MoniePay.src.services
+{
+    public class RegisterUser
+    {
+        public required string Username;
+        public required string Password;
+        public required string Email;
+        public RegisterUser(UserDetails userDetails)
+        {
+            Username = userDetails.username;
+            Password = userDetails.password;
+            Email = userDetails.email;
+        }
+    }
+    public sealed record UserDetails
+    (
+        string username,
+        string password,
+        string email
+    );
+}
