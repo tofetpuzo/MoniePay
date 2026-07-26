4b5b233ed619f2a28f19016b17a451276741de2b
tofetpuzo <tofet.femi@gmail.com>
2026-07-26 21:33:42 +0100

    feat: create payment intent

--- MoniePay/src/dto/CreatePaymentIntentRequest.cs
+++ MoniePay/src/dto/CreatePaymentIntentRequest.cs
@@ -6,9 +6,9 @@
  * Copyright (c) 2026, MoniePay
  */
 
+using MoniePay.src.models;
 using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
-using static MoniePay.src.models.Customer;
 namespace MoniePay.src.dto
 {
     public class CreatePaymentIntentRequest
@@ -19,9 +19,27 @@ namespace MoniePay.src.dto
 
         [JsonPropertyName("Currency")]
         [Required(ErrorMessage = "Currency is required")]
-        public required string Currency { get; set; }
+        public required Currency currency { get; set; }
 
-        public Customer customer { get; set; } 
+        [JsonPropertyName("customerId")]
+        [Required(ErrorMessage = "customerId is required")]
+        public Guid customerId { get; set; }
 
+        [JsonPropertyName("Amount")]
+        [Required(ErrorMessage = "Amount is required")]
+        public decimal Amount { get; set; }
+        public Channel channel { get; set; } = Channel.API;
+        public Status status { get; set; } = Status.PROCESSING;
+        public string IdempotencyKey { get; set; } = string.Empty;
+        public string Reference { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime LastUpdatedAt { get; set; }
+        //public Payments? Payment { get; set; }
+        // public Transactions? Transaction { get; set; }
+        // public Payouts? Payout { get; set; }
+        public PaymentAttempts? PaymentAttempts { get; set; }
     }
+
+    // look at the structure in db of payment and the payment-intent
+
 }
