8c074aa274208a151ad89a6feca13186686fce70
lola <54607597+tofetpuzo@users.noreply.github.com>
2026-09-19 17:49:16 +0100

    Update AdminController.cs

--- MoniePay/src/controller/AdminController.cs
+++ MoniePay/src/controller/AdminController.cs
@@ -1,4 +1,13 @@
-﻿using Microsoft.AspNetCore.Authorization;
+// SPDX-License-Identifier: Apache-2.0
+/*
+ * Customer onboarding: creates the Identity user and the customer profile
+ * atomically.
+ *
+ * Copyright (c) 2026, MoniePay
+ */
+
+
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MoniePay.src.dto;
 using MoniePay.src.services;
