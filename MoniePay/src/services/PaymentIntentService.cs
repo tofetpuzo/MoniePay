97c1a8fef920d0cd1b06de04b6b8fa59f1ab8738
lola <54607597+tofetpuzo@users.noreply.github.com>
2026-09-19 17:46:55 +0100

    Add license and copyright comments to PaymentIntentService
    Added SPDX license identifier and copyright notice.

--- MoniePay/src/services/PaymentIntentService.cs
+++ MoniePay/src/services/PaymentIntentService.cs
@@ -1,4 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+// SPDX-License-Identifier: Apache-2.0
+/*
+ * Customer onboarding: creates the Identity user and the customer profile
+ * atomically.
+ *
+ * Copyright (c) 2026, MoniePay
+ */
+
+using Microsoft.EntityFrameworkCore;
 using MoniePay.src.data;
 using MoniePay.src.dto;
 using MoniePay.src.models;
