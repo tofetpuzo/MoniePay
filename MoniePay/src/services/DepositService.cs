f623801324e81a6286c86f96d97f57c85b90170f
lola <54607597+tofetpuzo@users.noreply.github.com>
2026-09-19 17:46:20 +0100

    Add license and copyright comments to DepositService
    Added SPDX license identifier and copyright notice to DepositService.cs

--- MoniePay/src/services/DepositService.cs
+++ MoniePay/src/services/DepositService.cs
@@ -1,4 +1,12 @@
-﻿using MoniePay.src.dto;
+// SPDX-License-Identifier: Apache-2.0
+/*
+ * Customer onboarding: creates the Identity user and the customer profile
+ * atomically.
+ *
+ * Copyright (c) 2026, MoniePay
+ */
+
+using MoniePay.src.dto;
 
 namespace MoniePay.src.services
 {
